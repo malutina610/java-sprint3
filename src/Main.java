9615fbef1684f49a56afd4506b328f66440ade79
User <missis-malutina-yu@yandex.ru>
2024-03-15 16:17:05 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import model.Task;
+import service.TaskManager;
+
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        TaskManager taskManager = new TaskManager();
+        Task task = taskManager.create(new Task());
+
+        Task taskFromManager = taskManager.get(task.getId());
+
+        taskFromManager.setName("New name");
+
+        taskManager.update(taskFromManager);
+
+        taskManager.delete(taskFromManager.getId());
     }
 }
---
+++ src/service/TaskManager.java
@@ -0,0 +1,20 @@
+package service;
+
+import model.Task;
+
+public class TaskManager {
+    public Task create(Task task) {
+        return null;
+    }
+
+    public Task get(int id) {
+        return null;
+    }
+
+    public void update(Task taskFromManager) {
+    }
+
+    public void delete(int id) {
+
+    }
+}
